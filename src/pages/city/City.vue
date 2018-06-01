e557c3aec28c0f310fdaa0e924436d9b96b4d7c9
Misterbigbooo <278166530@qq.com>
2018-06-01 10:05:55 +0800

    city router

---
+++ src/pages/city/City.vue
@@ -0,0 +1,17 @@
+<template>
+  <city-header></city-header>
+</template>
+
+<script>
+import CityHeader from './components/Header'
+export default {
+  name: 'City',
+  components: {
+    CityHeader
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
