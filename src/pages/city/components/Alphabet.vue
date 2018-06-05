eda2e28ffe64c94a359e70e3bc239f94a298b249
Misterbigbooo <278166530@qq.com>
2018-06-05 14:14:50 +0800

    真机测试，修复字母表手势滑动bug

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -6,7 +6,7 @@
       :key="item"
       :ref="item"
       @click="handleLetterClick"
-      @touchstart="handleTouchStart"
+      @touchstart.prevent="handleTouchStart"
       @touchmove="handleTouchMove"
       @touchend="handleTouchEnd"
     >
